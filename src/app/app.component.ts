ac519d4549aeb40170227f3786659896ce11f79b
alirezaSoleymani <a83.soleimani@gmail.com>
2025-01-29 22:11:53 +0330

    component renamed to counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CountComponent } from './count/count.component';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CountComponent],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
