05cdd3874aee0803cf99cf72a2ebd2ca5b201eb1
alirezaSoleymani <a83.soleimani@gmail.com>
2025-01-29 21:51:39 +0330

    Create and use counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CountComponent } from './count/count.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CountComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngcourse';
